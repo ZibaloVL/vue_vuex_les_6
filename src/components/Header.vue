205d13077988ba6f2e6abb58d4e9a4409bf48172
ZibaloV <fotoroom.md@gmail.com>
2019-03-22 11:15:48 +0200

    собрал примерный шаблон

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <!-- 
+        <app-cart> </app-card>
+    -->
+</div>
+</template>
+
+<script>
+//import AppCart from './Cart';
+export default {
+    props:[],
+    components:{
+        
+    }
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+/*файл хранилища*/
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {/*data object* x:y*/
+
+  },
+
+  getters:{/*getters data object  x(state){return state.y}*/
+
+  },
+
+  mutations:{ /*action sinhr xAction(state, {z:s}){  state.xAction =... }*/ 
+
+  },
+
+  action:{/*axction asynhr sendX(store, data){store.commit('name_mutation')}*/
+
+  },
+
+  strict:true //жесткий режим чтобы не менять state напрямую 
+    // pocess.env.NODE_ENV !== 'production' если webpack
+
+
+})
